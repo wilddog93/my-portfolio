0f0ed584ae9597bf1b544aa015c42022b771e1c3
Jumakri Ridho Fauzi <ridhoajibx@gmail.com>
2025-01-31 04:47:56 +0700

    change

--- config/portfolio.ts
+++ config/portfolio.ts
@@ -36,7 +36,7 @@ import {
 // Website related settings
 const settings = {
   isSplash: true, // Change this to true if you want to use the splash screen.
-  useCustomCursor: false, // Change this to false if you want the good'ol cursor
+  useCustomCursor: true, // Change this to false if you want the good'ol cursor
   googleTrackingID: "UA-127555598-1",
 };
 
